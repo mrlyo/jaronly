286af245321e727763b1d1bbf0a514d333bac596
mrlyo <jannik_meier@yahoo.de>
2018-04-10 17:01:09 +0200

    added stuff

--- start_jars.bat
+++ start_jars.bat
@@ -1,1 +1,1 @@
-START "" /D C:\DEV\jarfromgit cmd /K "java -jar C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
+START "Start All Apps Script" /D C:\DEV\jarfromgit cmd /K "java -jar C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
