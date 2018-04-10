b7a253108f92b05250df8ccdf0fa67c8425c0eaf
mrlyo <jannik_meier@yahoo.de>
2018-04-10 17:24:26 +0200

    big change

--- start_jars.bat
+++ start_jars.bat
@@ -1,3 +1,1 @@
-cmd "cd C:\DEV\jarfromgit"
-START "Start All Apps Script" cmd  "java -jar C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
-pause
+START "Start All Apps Script" java -jar  "C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
