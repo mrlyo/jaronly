6b3c5c7949281e701faa284693a20f0f4d554502
mrlyo <36447035+mrlyo@users.noreply.github.com>
2018-04-11 09:48:34 +0200

    Update start_jars.bat

--- start_jars.bat
+++ start_jars.bat
@@ -1,2 +1,2 @@
-START "runas /user:JAMEIER"" cmd /K "cd C:\DEV\jarfromgit & java -jar C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
+START "runas /user:JAMEIER"" cmd /K "cd C:\DEV\jarfromgit & java C:\DEV\jarfromgit\sample-0.0.1-SNAPSHOT.jar"
 exit
